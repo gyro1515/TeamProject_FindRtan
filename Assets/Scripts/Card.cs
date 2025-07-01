0f0ba145cd887f13fc3e47ee89efdadcab44a6c4
Kseongmin1107 <ksmsky1107@gmail.com>
2025-07-01 11:17:06 +0900

    seongmin

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -59,7 +59,7 @@ public class Card : MonoBehaviour
     public void OpenCard()
     {
         // 게임 중이 아니라면 동작 금지
-        //if (GameManager.instance.gameStep != GameManager.GAMESTEP.STARTGAME) return;
+        if (GameManager.instance.progress != GameManager.GameProgress.StartGame) return;
 
         //PlayOneShot()을 사용하면 다른 효과음끼리 겹치지 않음
         audioSource.PlayOneShot(clip);
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,24 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public Button Retry;
     public static GameManager instance;
 
+    public enum GameProgress
+    {
+        EndGame, StartGame, Failed
+    }
 
-
+    public GameProgress progress = GameProgress.StartGame;
     public Card firstCard;
     public Card secondCard;
     public Text timeTxt;
     public GameObject endTxt;
-    public int cardCount = 0;
-    float time = 5.0f;
+    public int cardCount = 10;
+    float time = 30.0f;
     private void Awake()
     {
         if (instance == null)
@@ -24,8 +30,28 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
-        time -= Time.deltaTime;
-        timeTxt.text = time.ToString("N2");
+        switch (progress)
+        {
+            case GameProgress.EndGame:
+                SceneManager.LoadScene("GameOverScene");
+                Debug.Log("End");
+                break;
+            case GameProgress.StartGame:
+                time -= Time.deltaTime;
+               
+                if(time <= 0.0f)
+                {
+                    time = 0.0f;
+                    progress = GameProgress.Failed;
+                }
+                timeTxt.text = time.ToString("N2");
+                break;
+            case GameProgress.Failed:   
+                Retry.gameObject.SetActive(true);
+                break;
+            default:
+                break;
+        }
 
     }
 
@@ -36,9 +62,11 @@ public class GameManager : MonoBehaviour
             firstCard.DestroyCard();
             secondCard.DestroyCard();
             cardCount -= 2;
+            Debug.Log($"End{cardCount}");
             if (cardCount == 0)
             {
-                Time.timeScale = 0.0f;
+                progress = GameProgress.EndGame;
+                //Time.timeScale = 0.0f;
                 endTxt.SetActive(false);
             }
 
