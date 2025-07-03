4305ce9c83fd2173a35d5b85c67290d3a96791d2
gyro1515 <gyro1515@naver.com>
2025-07-03 15:43:09 +0900

    피피티 수정

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -8,14 +8,18 @@ using UnityEngine.UIElements;
 
 public class Board : MonoBehaviour
 {
+    // 프리팹 카드
     public GameObject card;
 
+    // 보간 사용을 위한 값 저장 배열
     List<Vector2> endV2;
     List<Vector2> startV2;
     List<float> startRot;
     List<GameObject> tmpG;
-    //Lerp용
+
+    //Lerp용 Time
     float lerpTime = 0.0f;
+
     // 사운드용
     AudioSource sound;
     // 사운드 속도
@@ -33,7 +37,7 @@ public class Board : MonoBehaviour
         if (sound != null) sound.pitch = pitchSpeed;
         // 날아가는 시간 세팅
         // 카드 날리는 간격 * 카드 인덱스만큼 + 날아가는 시간
-        cardTotalTime = cardTime * (cardCnt - 1) + 1.0f; 
+        cardTotalTime = cardTime * (cardCnt - 1) + 1.0f;
     }
     void Start()
     {
@@ -47,7 +51,7 @@ public class Board : MonoBehaviour
         // 기존 방식 = 원하는 위치에 소환
         /*for (int i = 0; i < 10; i++)
         {
-           GameObject go = Instantiate(card, this.transform);
+            GameObject go = Instantiate(card, this.transform);
 
             float x = (i % 4) * 1.4f - 2.1f;
             float y = (i / 4) * 1.4f - 3.0f;
@@ -61,7 +65,7 @@ public class Board : MonoBehaviour
         startV2 = new List<Vector2>(); // 시작 지점 저장용 배열
         startRot = new List<float>();  // 시작 회전 값 저장용 배열
         tmpG = new List<GameObject>(); // 카드 오브젝트 저장용 배열
-        
+
         for (int i = 0; i < cardCnt; i++)
         {
             tmpG.Add(Instantiate(card, this.transform));
@@ -79,7 +83,12 @@ public class Board : MonoBehaviour
             startRot.Add((float)i * 10 - 45f);
             // 시작 회전 게임 오브젝트에 설정하기
             tmpG[i].transform.Rotate(0f, 0f, startRot[i]);
+            Card tmpCard = tmpG[i].GetComponent<Card>();
             // 카드에 인덱스, 사진 등 세팅해주기
+            tmpCard.Setting(arr[i]);
+            tmpCard.anim.speed = 0f;
+            tmpCard.backSprite.sortingOrder = 20 - i;
+            tmpCard.backCanvas.sortingOrder = 20 - i;
             tmpG[i].GetComponent<Card>().Setting(arr[i]);
             // 카드 애니메이션 재생 중지하기
             tmpG[i].GetComponent<Card>().anim.speed = 0f;
@@ -121,7 +130,7 @@ public class Board : MonoBehaviour
                 // 0이하거나 1을 넘어가면 오류가 나기 때문에 범위 0부터 1까지만 지정하기
                 float t = Mathf.Clamp01(tmpLerpTime);
                 // 기본은 제곱으로 설정, 제곱 수 올릴 수록 처음에 빠르고, 마지막에 느려진다
-                float easedOut = 1 - Mathf.Pow(1 - t, 2); 
+                float easedOut = 1 - Mathf.Pow(1 - t, 2);
 
                 // 위치 보간하여 이동하기
                 tmpG[i].transform.position = Vector2.Lerp(startV2[i], endV2[i], easedOut);
@@ -146,6 +155,78 @@ public class Board : MonoBehaviour
                     tmpG[i].GetComponent<Card>().cardState = Card.CardState.Ready;
                 }
             }
-        } 
+        }
     }
+
+    // 주석 없는 버전
+    /*private void Awake()
+    {
+        sound = GetComponent<AudioSource>();
+        if (sound != null) sound.pitch = pitchSpeed;
+
+        cardTotalTime = cardTime * (cardCnt - 1) + 1.0f;
+    }
+    void Start()
+    {
+        int[] arr = { 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6 };
+        for (int i = arr.Length - 1; i > 0; i--)
+        {
+            int j = UnityEngine.Random.Range(0, i + 1);
+            (arr[i], arr[j]) = (arr[j], arr[i]);
+        }
+
+        endV2 = new List<Vector2>();   
+        startV2 = new List<Vector2>(); 
+        startRot = new List<float>();  
+        tmpG = new List<GameObject>(); 
+
+        for (int i = 0; i < cardCnt; i++)
+        {
+            tmpG.Add(Instantiate(card, this.transform));
+
+            float x = (i % 4) * 1.4f - 2.1f;
+            float y = (i / 4) * 1.4f - 3.0f;
+            endV2.Add(new Vector2(x, y));
+
+            tmpG[i].transform.position = new Vector2(0f, -5f);
+            startV2.Add(tmpG[i].transform.position);
+            startRot.Add((float)i * 10 - 45f);
+            tmpG[i].transform.Rotate(0f, 0f, startRot[i]);
+            Card tmpCard = tmpG[i].GetComponent<Card>();
+            tmpCard.Setting(arr[i]);
+            tmpCard.anim.speed = 0f;
+            tmpCard.backSprite.sortingOrder = 20 - i;
+            tmpCard.backCanvas.sortingOrder = 20 - i;
+        }
+        endV2.Reverse();
+        startV2.Reverse();
+    }
+    void Update()
+    {
+        if (lerpTime >= 0.0f)
+        {
+            lerpTime += Time.deltaTime;
+            for (int i = 0; i < cardCnt; i++) 
+            {
+                int tmpI = cardTime == 0.0f ? cardCnt : (int)(lerpTime / cardTime);
+                if (tmpI < i) return;
+                float tmpLerpTime = lerpTime - cardTime * i;
+                float t = Mathf.Clamp01(tmpLerpTime);
+                float easedOut = 1 - Mathf.Pow(1 - t, 2);
+                tmpG[i].transform.position = Vector2.Lerp(startV2[i], endV2[i], easedOut);
+                float angleOffset = Mathf.Lerp(0f, 720f - startRot[i], easedOut);
+                float nextZ = startRot[i] + angleOffset;
+                tmpG[i].transform.rotation = Quaternion.Euler(0f, 0f, nextZ);
+            }
+            if (lerpTime > cardTotalTime)
+            {
+                lerpTime = -1f;
+                for (int i = 0; i < cardCnt; i++)
+                {
+                    tmpG[i].GetComponent<Card>().anim.speed = 1.0f;
+                    tmpG[i].GetComponent<Card>().cardState = Card.CardState.Ready;
+                }
+            }
+        }
+    }*/
 }
